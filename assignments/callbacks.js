0c34801c63909cffc0ac93d3ef6e54e57cba65d4
schang1146 <sammychang2185@gmail.com>
2019-03-31 18:25:45 -0700

    finished callbacks.js

--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -27,29 +27,77 @@ const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
 
 function getLength(arr, cb) {
   // getLength passes the length of the array into the callback.
+  return cb(arr.length);
 }
+
+getLength(items, function(result) {
+  console.log(result);
+})
 
 function last(arr, cb) {
   // last passes the last item of the array into the callback.
+  return cb(arr[arr.length - 1]);
 }
+
+last(items, function(result) {
+  console.log(result);
+})
 
 function sumNums(x, y, cb) {
   // sumNums adds two numbers (x, y) and passes the result to the callback.
+  return cb(x + y);
 }
+
+sumNums(3, 4, function(result) {
+  console.log(result);
+})
 
 function multiplyNums(x, y, cb) {
   // multiplyNums multiplies two numbers and passes the result to the callback.
+  return cb(x * y);
 }
+
+multiplyNums(3, 7, function(result) {
+  console.log(result);
+})
 
 function contains(item, list, cb) {
   // contains checks if an item is present inside of the given array/list.
   // Pass true to the callback if it is, otherwise pass false.
+  for (let i = 0; i < list.length; i++) {
+    if (item == list[i]) {
+      return cb(true);
+    }
+  }
+  return cb(false);
 }
 
+contains('Pencil', items, function(result) {
+  console.log(result);
+})
+contains('Bomb', items, function(result) {
+  console.log(result);
+})
+
 /* STRETCH PROBLEM */
+
+const hasDuplicates = [1, 3, 0, 3, 3, 'apple', 'banana', 'random', 'banana'];
 
 function removeDuplicates(array, cb) {
   // removeDuplicates removes all duplicate values from the given array.
   // Pass the duplicate free array to the callback function.
   // Do not mutate the original array.
+  unique = [];
+  for (let i = 0; i < array.length; i++) {
+    if (!contains(array[i], unique, function(result) {
+      return result;
+    })) {
+      unique.push(array[i]);
+    }
+  }
+  return cb(unique);
 }
+
+removeDuplicates(hasDuplicates, function(result) {
+  console.log(result);
+})
